042023b4f77f82d62ef0a150f4474413107dc7b9
minhvu278 <dominhvu278@gmail.com>
2025-02-20 23:24:31 +0700

    listen port

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('DATABASE_HOST:', process.env.DATABASE_HOST);
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
