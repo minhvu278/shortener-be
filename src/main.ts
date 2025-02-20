5ebd3117a28a84efde830c82ac5d0faf81e67710
minhvu278 <dominhvu278@gmail.com>
2025-02-20 23:47:20 +0700

    fix port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  await app.listen(4000, '0.0.0.0');
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
